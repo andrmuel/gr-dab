40cbcfc0adf93727f5a685feb931eac58caae3bf
Andreas Mueller <am@0x7.ch>
2009-01-03 14:02:59 +0000

    forgot to add FIC protocal defs

---
+++ src/lib/FIC.h
@@ -0,0 +1,20 @@
+#ifndef _FIC_H
+#define _FIC_H 
+
+#define FIB_LENGTH 32
+#define FIB_CRC_LENGTH 2
+#define FIB_CRC_POLY 0x1021
+#define FIB_CRC_INITSTATE 0xffff
+#define FIB_ENDMARKER 0xff
+
+#define FIB_TYPE_MCI    0
+#define FIB_TYPE_LABEL1 1
+#define FIB_TYPE_LABEL2 2
+#define FIB_TYPE_FIDC   5
+#define FIB_TYPE_CA     6
+
+#define FIB_FIDC_EXTENSION_PAGING 0
+#define FIB_FIDC_EXTENSION_TMC    1
+#define FIB_FIDC_EXTENSION_EWS    2
+
+#endif /* _FIC_H */
